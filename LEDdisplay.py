b8bf43172b33915a1adb54225e2b89bc3705130b
yjq040128 <2271427187@qq.com>
2025-12-08 18:35:59 +0800

    first time, for LED

---
+++ LEDdisplay.py
@@ -0,0 +1,22 @@
+
+from microbit import display, Image, sleep
+
+display.show("TEST")
+sleep(1000)
+display.clear()
+
+while True:
+
+    display.show("0")
+    sleep(1000)
+
+
+    display.show("1")
+    sleep(1000)
+
+    display.show("2")
+    sleep(1000)
+
+
+    display.clear()
+    sleep(1000)
